d9f7500a8874060b9a1cd6e2fe6f327e39699614
Raghav Bodicherla <raghav.bodicherla@gmail.com>
2024-11-21 19:13:37 -0800

    makes working function

--- bob.py
+++ bob.py
@@ -5,14 +5,15 @@ from pybricks.robotics import DriveBase
 from pybricks.tools import wait
 
 class Bob:
-    hub = any
-    drivebase = any
-    top_motor_left = any
-    top_motor_right = any
-    bottom_motor_left= any
-    bottom_motor_right = any
-    color_sensor = any
-    front_motor = any
+    hub = PrimeHub()
+    top_motor_left = Motor(Port.E)
+    top_motor_right = Motor(Port.F, positive_direction=Direction.COUNTERCLOCKWISE)
+    bottom_motor_left = Motor(Port.C, positive_direction=Direction.COUNTERCLOCKWISE)
+    bottom_motor_right = Motor(Port.D)
+    color_sensor = ColorSensor(Port.B)
+    front_motor = Motor(Port.A)
+    drivebase = DriveBase(bottom_motor_left, bottom_motor_right, 50, 145)
+    speed = 300
 
     def __init__(self):
         hub = PrimeHub()
@@ -24,21 +25,32 @@ class Bob:
         front_motor = Motor(Port.A)
         drivebase = DriveBase(bottom_motor_left, bottom_motor_right, 50, 145)
 
-    def move(self, distance):
-        print("move fwd " + str(distance))
-        stop_everything()
-        bottom_motor_left.run(-30)
-        bottom_motor_right.run(-30)
-        wait(3000)
+    def forward(distance, speed):
+        bottom_motor_left.run(22.55 * speed)
+        bottom_motor_right.run(22.55 * speed)
+        wait(1000*distance/speed)
+        stopall()
 
-    def turnLeft(self, degree):
-        print("turning left " + str(degree))
+    def turnleft(time, distance):
+        bottom_motor_left.run(-1*distance)
+        bottom_motor_right.run(distance)
+        wait(time)
 
-    def turnRight(self, degree):
-        print("turning right " + str(degree))
+    def turnright(time, distance):
+        bottom_motor_left.run(distance)
+        bottom_motor_right.run(-1*distance)
+        wait(time)
+        stopall()
 
-    def stop_everything():
-        top_motor_left.stop()
-        bottom_motor_right.stop()
+    def reverse(distance, speed):
+        bottom_motor_left.run(-22.55 * speed)
+        bottom_motor_right.run(-22.55 * speed)
+        wait(1000*distance/speed)
+        stopall()
+
+    def stopall():
         bottom_motor_left.stop()
+        bottom_motor_right.stop()
+        front_motor.stop()
+        top_motor_left.stop()
         top_motor_right.stop()
