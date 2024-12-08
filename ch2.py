f4e82e8404b550909102a0bab6908c116e606827
Raghav Bodicherla <raghav.bodicherla@gmail.com>
2024-12-07 18:20:19 -0800

    made working picker upers

--- ch2.py
+++ ch2.py
@@ -2,6 +2,5 @@ import bob
 
 def solve(bob):
     print("running challenge 2")
-    bob.turnLeft(90)
-    bob.move(109)
+    
     print("end challenge 2")
--- main.py
+++ main.py
@@ -11,5 +11,34 @@ ch1.solve(bob)
 
 ch2.solve(bob)
 
+# fetch 1st one
+bob.turn(-6)
+bob.forward(360)
+bob.topmotors(-100)
+
+#fetch 1st krill 
+bob.forward (50)
+bob.turn(60)
+bob.turn(-50)
+bob.forward(58)
+bob.topmotors(100)
+bob.forward (30)
+bob.topmotors(-100)
+#bob.reverse(550)
+# e(500)
+
+#fetch 2nd krill
+bob.turn(-20)
+bob.forward(72)
+bob.topmotors(100)
+bob.forward(55)
+bob.topmotors(-100)
+
+#come back to position
+bob.reverse(600)
+
+#bob.turn(-80)
 bob.beep(500, 500)
+
+
 print("Program has ended")
