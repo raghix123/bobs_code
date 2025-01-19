328269bca501bf7986238999e491162802760f54
Raghav Bodicherla <raghav.bodicherla@gmail.com>
2025-01-18 16:53:15 -0800

    WIP, Raghav;s collect objects code

--- collect_objects.py
+++ collect_objects.py
@@ -2,20 +2,6 @@ import bob
 
 
 def solve(bob):
-    # bob.turn(-12)
-    # bob.forward(360)
-    # bob.topmotors(-91)
-    # bob.turn(28)
-    # bob.topmotors(90)
-    # bob.forward(120)
-    # bob.topmotors(-90)
-    # bob.turn(-25)
-    # bob.forward(100)
-    # bob.topmotors(90)
-    # bob.forward(100)
-    # bob.turn(45)
-    # bob.forward(25)
-    # bob.topmotors(-90)
 
     bob.turn(-12)
     bob.forward(360)
@@ -34,3 +20,15 @@ def solve(bob):
     bob.topmotors(-90)
     bob.turn(-50)
     bob.reverse(750)
+    bob.topmotors(180)
+    #next
+    bob.forward(200)
+    bob.turn(-45)
+    bob.forward(325)
+    bob.reverse(155)
+    bob.turn(90)
+    bob.frontmotor(-10)
+    bob.forward(260)
+    bob.frontmotor(-200)
+    bob.turn(-45)
+    bob.reverse(400)
