4c6e82c3af05ca2c5179eb54429797d815059db3
Raghav Bodicherla <raghav.bodicherla@gmail.com>
2025-01-25 17:35:36 -0800

    Worked on kraken and banana boat, cold seep

--- ch9.py
+++ ch9.py
@@ -1,8 +1,27 @@
 import bob
 
-def solve(bob):
-    bob.forward(200)
-    bob.turn(-34)
+
+def solve (bob):
+    #moving to banana boat
+    bob.forward(405)
+    bob.turn(45)
+    bob.forward(82)
+    bob.frontmotor(-190)
+    bob.reverse(300)
+
+    #octopus challenge
+    bob.frontmotor(190)
+    bob.topmotors(-195)
+    bob.turn(-120)
+    #fetch octopus
+    bob.forward(125)
+    bob.reverse(155)
+
+    #move the octopus
+    bob.turn(-80)
     bob.forward(300)
-    bob.reverse(200)
     bob.turn(90)
+    bob.forward(200)
+#doing kraken challenge
+    #bob.reverse(200)
+    #bob.turn(180)
