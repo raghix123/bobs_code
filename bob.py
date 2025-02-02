047450a5aab1ae258d4155d44350c8beb35ba376
Vinay Prakash <prakvinay@gmail.com>
2025-02-01 16:30:01 -0800

    initialized front motor in bob.py; previously not initialized

--- bob.py
+++ bob.py
@@ -21,7 +21,7 @@ class Bob:
         self.bottom_motor_left = Motor(Port.C, positive_direction=Direction.COUNTERCLOCKWISE)
         self.bottom_motor_right = Motor(Port.D)
         #self.color_sensor = ColorSensor(Port.B)
-        #self.front_motor = Motor(Port.A)
+        self.front_motor = Motor(Port.A)
         self.drivebase = DriveBase(self.bottom_motor_left, self.bottom_motor_right, 56, 143)
         self.hub.speaker.volume(100)
         # self.drivebase.use_gyro(True)
