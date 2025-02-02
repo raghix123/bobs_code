25ee254ec753c6ddcb9fa0ddc64c7c9f7d9ba8ba
Vinay Prakash <prakvinay@gmail.com>
2025-02-01 17:47:21 -0800

    Fixed up challenge 9 file to do banana boat, octopus, and cold seep

--- ch9.py
+++ ch9.py
@@ -3,25 +3,28 @@ import bob
 
 def solve (bob):
     #moving to banana boat
+    bob.turn(1)
     bob.forward(405)
     bob.turn(45)
-    bob.forward(82)
+    bob.forward(85)
     bob.frontmotor(-190)
     bob.reverse(300)
 
     #octopus challenge
     bob.frontmotor(190)
     bob.topmotors(-195)
-    bob.turn(-120)
+    bob.turn(-80)
     #fetch octopus
     bob.forward(125)
     bob.reverse(155)
 
     #move the octopus
-    bob.turn(-80)
-    bob.forward(300)
-    bob.turn(90)
+    bob.turn(-50)
     bob.forward(200)
+    bob.turn(35)
+    bob.forward(300)
+    bob.turn(50)
+    bob.forward(30)
 #doing kraken challenge
     #bob.reverse(200)
     #bob.turn(180)
