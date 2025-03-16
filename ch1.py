906eadbc191de4c91f0f98abace9adee783baad2
Rishaan Krishnan <rishaan.krishnan@gmail.com>
2025-03-15 18:22:16 -0700

    fixed robot and made it do half on ch1

--- ch1.py
+++ ch1.py
@@ -6,46 +6,45 @@ def moveIntoPosition(bob):
 
 def solve(bob):
     # # # # Run to Coral Tree
-    bob.forward(260)
+    bob.forward(261)
 
     # # # # # Solve Coral Tree
     bob.forward_and_left_front(75,60)
     bob.forward(75)
     bob.leftmotor(20)
-    bob.forward(40)
-    bob.leftmotor(15)
+    bob.forward(45)
+    bob.leftmotor(25)
     bob.forward(65)
-    bob.leftmotor(-65)
+    bob.leftmotor(-80)
 
     # # # # # Reversing away from Coral Tree
-    bob.reverse(50)
+    bob.reverse(70)
 
     # # # # # Run to Coral Buds
     bob.turn(90)
-    bob.forward(300)
+    bob.forward(299)
     bob.turn(-90)
-    bob.forward(352)
+    bob.forward(303)
     bob.turn(-90)
 
-    # # # # # Solve Coral Buds
-    bob.forward(22)
-    bob.leftmotor(-33)
+    # # # Solve Coral Buds
+    bob.leftmotor(-40,999999)
     bob.forward(65)
     bob.leftmotor(50)
 
     # # # # Solve Mr. Diddyghav(aka sharky boi)
-    bob.turn(49.6)
-    bob.forward(43)
-    bob.leftmotor(-63)
+    bob.turn(50)
+    bob.forward(20)
+    bob.leftmotor(-60,999999)
 
     # #go back to spawn
-    bob.leftmotor(40)
-    bob.reverse(50)
-    bob.turn(50)
-    bob.reverse(700)
+    # bob.leftmotor(40)
+    # bob.reverse(50)
+    # bob.turn(50)
+    # bob.reverse(700)
     
     # Solve Raise the mast
-    bob.forward(550)
-    bob.turn(95)
-    bob.forward(40)
-    bob.leftmotor(200)
+    # bob.forward(550)
+    # bob.turn(95)
+    # bob.forward(40)
+    # bob.leftmotor(200)
